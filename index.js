86e81f352d6b3f0fa98468202b4914ea1734e528
menna-tolla <mennatullah267@gmail.com>
2024-10-21 00:27:27 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,158 @@
+
+// solving1-----------------
+// var number = Number(window.prompt("enter number"));
+// console.log("number is : " + number);
+// -----------------------------------------------------------------------------------
+
+// solving2------------------
+// var number = Number(window.prompt("enter number"));
+// if(number%3==0 && number%4==0)
+// {
+//     console.log("yase");
+// }
+// else
+// {
+//     console.log("NO");
+// }
+// ----------------------------------------------------------------------------------------
+
+
+// solvinv3-----------------------------
+// var x1 =Number(window.prompt("enter numberOne"));
+// var x2 =Number(window.prompt("enter numberTow"));
+// if(x1>x2)
+// {
+//     console.log(x1);
+// }
+// else
+// {
+//     console.log(x2);
+// }
+// -------------------------------------------------------------------------------------------
+
+
+// solving4----------------------------------
+// var number =Number(window.prompt("enter numberOne"));
+// if(number>0)
+// {
+//     console.log("positive");
+// }
+// else
+// {
+//     console.log("negative");
+// }
+// -------------------------------------------------------------------------------------------------
+
+// solving5
+// var n1 =Number(window.prompt("enter number one"));
+// var n2 =Number(window.prompt("enter scond number"));
+// var n3 =Number(window.prompt("enter third number"));
+// if(n1>n2 && n1>n3)
+// {
+//     console.log("max element = " + n1)
+//     if(n2> n3)
+//     {
+//         console.log("min element = " + n3)
+//     }
+//     else 
+//     {
+//         console.log("min element = " + n2)
+//     }
+// }
+// else if(n2>n1 && n2>n3)
+// {
+//     console.log("max element = " + n2)
+//     if(n1> n3)
+//     {
+//         console.log("min element = " + n3)
+//     }
+//     else 
+//     {
+//         console.log("min element = " + n1)
+//     }
+// }
+// else if(n3>n1 && n3>n2)
+// {
+//     console.log("max element = " + n3)
+//     if(n2> n1)
+//     {
+//         console.log("min element = " + n1)
+//     }
+//     else 
+//     {
+//         console.log("min element = " + n2)
+//     }
+// }
+// -----------------------------------------------------------------------------------------------------
+
+// solving6
+// var x=Number(window.prompt("enter the number"));
+// if(x%2==0)
+// {
+//     console.log("number is even");
+// }
+// else 
+// {
+//     console.log("number is odd");
+// }
+// ---------------------------------=======-jdlksdk-------------------------------------------------------------------
+
+// solving8
+var char =window.prompt("enter the char");
+switch(true)
+{
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+        console.log("vowel");
+        break;
+        defualt:
+        console.log("Consonant");
+}
+// -------------------------------------------------------------------------------------------
+
+//  solving9
+
+// var x=Number(window.prompt("enter the number"));
+// for(var i=1 ; i<=x ; i++)
+// {
+//     console.log(i);
+// }
+
+// --------------------------------------------------------------------------------------------
+// solving10
+
+// var x=Number(window.prompt("enter the number"));
+// for(var i=1 ; i<=12 ;i++)
+// {
+//     console.log(i*x);
+// }
+
+// ----------------------------------------------------------------------------------------------
+// solving11
+// var x=Number(window.prompt("enter the number"));
+// for(var i=1 ; i<=x ; i++)
+// {
+//     if(i%2==0)
+//     {
+//         console.log(i);
+//     }
+// }
+// -------------------------------------==============================)(------------------------------------------
+ 
+// solving12
+// var x=Number(window.prompt("enter the x number"));  //4
+// var z=Number(window.prompt("enter the z number"));  //3
+// console.log(x);
+// var result=0;
+// for(var i=0 ; i<=z ; i++)
+// {
+//     result=x*x;    //5 10 15
+// }
+// console.log(result);
+// ---------------------------------------------------------------------------------------
+// solving13
+
+
